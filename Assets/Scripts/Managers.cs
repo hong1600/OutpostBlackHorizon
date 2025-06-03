9591f673970e7d2345d612061db520e4d4156d27
hong1600 <esse5695@naver.com>
2025-06-04 03:17:27 +0900

    테스트

--- Assets/Scripts/Managers.cs
+++ Assets/Scripts/Managers.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,5 +8,21 @@ public class Managers : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            CheckPlayer();
+        }
+    }
+
+    private void CheckPlayer()
+    {
+        foreach (var player in PhotonNetwork.CurrentRoom.Players)
+        {
+            Debug.Log($"플레이어 ID: {player.Value.ActorNumber}, 닉네임: {player.Value.NickName}");
+        }
     }
 }
